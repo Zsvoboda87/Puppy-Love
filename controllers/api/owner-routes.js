8510e2605f6eac8e804c1e480c46733e7e110834
Zsvob <svobodazachary@gmail.com>
2022-02-28 10:49:37 -0500

    server setup, login routes

---
+++ controllers/api/owner-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  });
+
+  module.exports = router;
