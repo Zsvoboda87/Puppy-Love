39c1895ec9cf07f349a655f017b91224bedb7ce5
DCT <mirachan2332@gmail.com>
2022-03-03 11:14:46 -0600

    first push Diana branch, working on pet-routes

--- controllers/api/pet-routes.js
+++ controllers/api/pet-routes.js
@@ -1,7 +1,26 @@
 const router = require('express').Router();
+const sequelize = require('../..config/connection');
+const { Pet, Owner } = require('../../models');
 
-
-// pets routes go here
+//get all pets
+router.get('/', (req, res) => {
+    console.log('=================');
+    Pet.findAll({
+        attributes: [
+            'image',
+            'id',
+            'petOwner',
+            'petName',
+            'petGender',
+            'petBirthday',
+            'petLikes'
+        ]
+    }).then(puppy_love_db => res.json(puppy_love_db))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 
 
