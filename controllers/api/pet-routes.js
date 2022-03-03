8754acaf32d1da99d6998ee939f83d5a1533f4ae
DCT <mirachan2332@gmail.com>
2022-03-03 11:18:59 -0600

    Pet.findAll and Pet.findOne implemented

--- controllers/api/pet-routes.js
+++ controllers/api/pet-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../..config/connection');
 const { Pet, Owner } = require('../../models');
+const res = require('express/lib/response');
 
 //get all pets
 router.get('/', (req, res) => {
@@ -22,6 +23,32 @@ router.get('/', (req, res) => {
     });
 });
 
-
+//get specific pet by id
+router.get('/:id', (req, res) => {
+    Pet.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'image',
+            'id',
+            'petOwner',
+            'petName',
+            'petGender',
+            'petBirthday',
+            'petLikes'
+        ]
+    }).then(puppy_love_db => {
+        if (!puppy_love_db) {
+            res.status(404).json({ message: 'No pet found with this id' });
+            return;
+        }
+        res.json(puppy_love_db);
+    })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
