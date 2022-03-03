14d84c9b8405ddbe72e8230f9d6324c81992a644
Maria <mtornabene05@gmail.com>
2022-03-03 18:44:03 -0500

    addition of prettier

--- controllers/api/pet-routes.js
+++ controllers/api/pet-routes.js
@@ -1,141 +1,144 @@
-const router = require('express').Router();
-const sequelize = require('../..config/connection');
-const { Pet, Owner } = require('../../models');
-const res = require('express/lib/response');
+const router = require("express").Router();
+// const sequelize = require('../..config/connection');
+const { Pet, Owner } = require("../../models");
+const res = require("express/lib/response");
 
 //get all pets
-router.get('/', (req, res) => {
-    console.log('=================');
-    Pet.findAll({
-        attributes: [
-            'image',
-            'id',
-            'petOwner',
-            'petName',
-            'petGender',
-            'petBirthday',
-            'petLikes'
-        ]
-    }).then(puppy_love_db => res.json(puppy_love_db))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+router.get("/", (req, res) => {
+  console.log("=================");
+  Pet.findAll({
+    attributes: [
+      "image",
+      "id",
+      "petOwner",
+      "petName",
+      "petGender",
+      "petBirthday",
+      "petLikes",
+    ],
+  })
+    .then((puppy_love_db) => res.json(puppy_love_db))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
 //get specific pet by id
-router.get('/:id', (req, res) => {
-    Pet.findOne({
-        where: {
-            id: req.params.id
-        },
-        attributes: [
-            'image',
-            'id',
-            'petOwner',
-            'petName',
-            'petGender',
-            'petBirthday',
-            'petLikes'
-        ]
-    }).then(puppy_love_db => {
-        if (!puppy_love_db) {
-            res.status(404).json({ message: 'No pet found with this id' });
-            return;
-        }
-        res.json(puppy_love_db);
+router.get("/:id", (req, res) => {
+  Pet.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: [
+      "image",
+      "id",
+      "petOwner",
+      "petName",
+      "petGender",
+      "petBirthday",
+      "petLikes",
+    ],
+  })
+    .then((puppy_love_db) => {
+      if (!puppy_love_db) {
+        res.status(404).json({ message: "No pet found with this id" });
+        return;
+      }
+      res.json(puppy_love_db);
     })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 //create new pet
-router.post('/', (req, res) => {
-    Pet.create({
-        image: req.body.image,
-        id: req.session.id,
-        petOwner: req.body.petOwner,
-        petName: req.body.petName,
-        petGender: req.body.petGender,
-        petBirthday: req.body.petBirthday,
-        petLikes: req.body.petLikes
-    }).then(puppy_love_db => res.json(puppy_love_db))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+router.post("/", (req, res) => {
+  Pet.create({
+    image: req.body.image,
+    id: req.session.id,
+    petOwner: req.body.petOwner,
+    petName: req.body.petName,
+    petGender: req.body.petGender,
+    petBirthday: req.body.petBirthday,
+    petLikes: req.body.petLikes,
+  })
+    .then((puppy_love_db) => res.json(puppy_love_db))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
 //update specific pet's image
-router.put('/:id', (req, res) => {
-    Pet.update(
-        {
-            image: req.body.image
-        },
-        {
-            where: {
-                id: req.params.id
-            }
-        }
-    )
-    .then(puppy_love_db => {
-        if (!puppy_love_db) {
-            res.status(404).json({ message: 'No pet found with this id' });
-            return;
-        }
-        res.json(puppy_love_db);
+router.put("/:id", (req, res) => {
+  Pet.update(
+    {
+      image: req.body.image,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((puppy_love_db) => {
+      if (!puppy_love_db) {
+        res.status(404).json({ message: "No pet found with this id" });
+        return;
+      }
+      res.json(puppy_love_db);
     })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
 //update specific pet's likes
-router.put('/:id', (req, res) => {
-    Pet.update(
-        {
-            petLikes: req.body.petLikes
-        },
-        {
-            where: {
-                id: req.params.id
-            }
-        }
-    )
-    .then(puppy_love_db => {
-        if (!puppy_love_db) {
-            res.status(404).json({ message: 'No pet found with this id' });
-            return;
-        }
-        res.json(puppy_love_db);
+router.put("/:id", (req, res) => {
+  Pet.update(
+    {
+      petLikes: req.body.petLikes,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((puppy_love_db) => {
+      if (!puppy_love_db) {
+        res.status(404).json({ message: "No pet found with this id" });
+        return;
+      }
+      res.json(puppy_love_db);
     })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
 //delete a specific pet from the owner
-router.delete('/:id', (req, res) => {
-    console.log('id', req.params.id);
-    Pet.destroy({
-        where: {
-            id: req.params.id
-        }
+router.delete("/:id", (req, res) => {
+  console.log("id", req.params.id);
+  Pet.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((puppy_love_db) => {
+      if (!puppy_love_db) {
+        res.status(404).json({ message: "No pet found with this id" });
+        return;
+      }
+      res.json(puppy_love_db);
     })
-    .then(puppy_love_db => {
-        if (!puppy_love_db) {
-            res.status(404).json({ message: 'No pet found with this id' });
-            return;
-        }
-        res.json(puppy_love_db);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
